6da5447e7cdb29e9d81cc3dd50b72fc4e6a304a9
icerchill <qibingicer@gmail.com>
2015-11-25 23:18:45 -0500

    Update ControllerGui.py
    Initial GUI frame work

--- Controller_Pi/ControllerGui.py
+++ Controller_Pi/ControllerGui.py
@@ -1,1 +1,61 @@
 #This is a controller GUI Program
+
+from gi.repository import Gtk
+
+class MainWindow(Gtk.Window):
+
+    def __init__(self):
+        """
+        constructor, generates the base layout of the central control unit
+        :return:
+        """
+        Gtk.Window.__init__(self, title="Central Control")
+        self.set_default_size(400, 400)
+        self.set_default_geometry
+        self.set_border_width(10)
+        listbox = Gtk.ListBox()
+        listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        self.add(listbox)
+
+        reset_row = Gtk.ListBoxRow()
+        reset_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=100)
+        reset_row.add(reset_box)
+        reset_label = Gtk.Label("Reset the whole system")
+        reset_label.set_markup("<big>Reset the whole system</big>")
+        reset_button = Gtk.Button("Reset")
+        reset_box.pack_start(reset_label, True, True, 0)
+        reset_box.pack_start(reset_button, True, True, 0)
+        listbox.add(reset_row)
+
+        led_row1 = Gtk.ListBoxRow()
+        led_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=100)
+        led_row1.add(led_box)
+        led1_label = Gtk.Label("Room 1 LED Status")
+        led_button_room_1 = Gtk.Switch()
+        led_box.pack_start(led1_label, True, True, 0)
+        led_box.pack_start(led_button_room_1, True, True, 0)
+        listbox.add(led_row1)
+
+        led_row2 = Gtk.ListBoxRow()
+        led_box2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=100)
+        led_row2.add(led_box2)
+        led1_label2 = Gtk.Label("Room 2 LED Status")
+        led_button_room_2 = Gtk.Switch()
+        led_box2.pack_start(led1_label2, True, True, 0)
+        led_box2.pack_start(led_button_room_2, True, True, 0)
+        listbox.add(led_row2)
+
+        song_row = Gtk.ListBoxRow()
+        song_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=100)
+        song_row.add(song_box)
+        song_label = Gtk.Label("Song status:")
+
+        song_box.pack_start(song_label, True, True, 0)
+        #song_box.pack_start(led_button_room_2, True, True, 0)
+        listbox.add(song_row)
+
+win = MainWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
+
