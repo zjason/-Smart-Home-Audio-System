1510c2b64693605f3615064551701378eecef569
windflow <windflow@users.noreply.github.com>
2015-12-06 13:26:56 -0500

    Update and rename music_mp3_player.py to music_player.py
    some details added

--- Room_Pi/music_player.py
+++ Room_Pi/music_player.py
@@ -1,6 +1,11 @@
-# play a MP3 music file using module pygame
+# play a music file using module pygame
 # (does not create a GUI frame for now)
 # Muzi
+
+import sys
+import pygame as pg
+
+#play a list in order
 
 import sys
 import pygame as pg
@@ -19,7 +24,7 @@ class music_player():
         #file.close()
         print "playlist",self.play_list
         song = 0
-        self.count = 0
+        self.index = 0
         song_volume = 0.2 #default volume
         pg.init()#init pygame
         #pg.display.set_mode((500,500)) #menu window
@@ -42,11 +47,12 @@ class music_player():
                     pg.mixer.music.play()
 
 
-
+    #Let user change volume
     def change_volume(self,vol):
         song_volume = vol
         pg.mixer.music.set_volume(song_volume)
 
+    #activate the player and start playing music
     def activate_player(self,activate_command):
         if activate_command == 'activate':
             print"Player activated"
@@ -54,11 +60,13 @@ class music_player():
             self.music_status = "playing"
         print "Play awaiting"
 
-
+    #play the next song in list
     def go_next(self):
         print"Playing next song~~"
-        self.count+=1
-        self.track_name = self.play_list[self.count+1] #track the next song name
+        self.index+=1
+        self.track_name = self.play_list[self.index+1] #track the next song name
+
+    #pause it
     def pause(self):
         """
         Pauses the current song.
@@ -68,6 +76,13 @@ class music_player():
         pg.mixer.music.pause()
         self.playing = False
 
+    def un_pause(self):
+        pg.mixer.music.unpause()
+        self.music_status = "playing"
+        #self.playing = True # Does it work? or need to reload music?
+        #pg.mixer.music.load(self.play_list[self.index])#start from the very first song
+        #pg.mixer.music.play()
+
     def stop(self):
         #Stops the current song and goes to the beginning.
         pg.mixer.music.stop()
@@ -76,16 +91,28 @@ class music_player():
     def previousSong(self):
         #Returns to the previous song in the playlist.
         if self.play_list:
-            self.count -= 1
-            if self.count < 0:
-                self.count = len(self.play_list) - 1
+            self.index -= 1
+            if self.index < 0:
+                self.index = len(self.play_list) - 1
             self.stop()
-        self.track_name = self.play_list[self.count]
+        self.track_name = self.play_list[self.index]
 
             #self.loadSong()
 
+    def nextSong(self):
+        if self.play_list:
+            playNextSong = self.playing
+            self.index += 1
+            if self.index >= len(self.play_list):
+                self.index = 0
+                #if not self.loop:
+                    #playNextSong = False
+            self.stop()
+            #self.loadSong()
+            if playNextSong:
+                self.play()
 
 
 
 
-l = music_player() #TEst to call this player class
+music_test = music_player() #TEst to call this player class
